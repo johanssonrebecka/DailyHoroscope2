5a2c0358f085e68d4fd1afdee87beb2d1ebe012b
johanssonrebecka <43539086+johanssonrebecka@users.noreply.github.com>
2019-05-02 11:54:55 +0200

    Add files via upload

---
+++ precache-manifest.917b78858715cfe6f5d518edd2c29bfc.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dfa9fdbafb545f5dc738122a1ffc3c5",
+    "url": "/static/media/StartScreen_elImage3_418817.5dfa9fdb.png"
+  },
+  {
+    "revision": "7c50d59ca1e2c90a6d1c",
+    "url": "/static/css/main.2d42a1ef.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "34fa2d1bda286f33dbad",
+    "url": "/static/js/2.682214f6.chunk.js"
+  },
+  {
+    "revision": "7f70c89624a812e3ea796471b3ed1712",
+    "url": "/static/media/Aquarius2Screen_elJeremythomas98201unsplash_227519.7f70c896.jpg"
+  },
+  {
+    "revision": "5294a9e9129750ad4d2d2a08e4d5a6e4",
+    "url": "/static/media/Didot.5294a9e9.ttf"
+  },
+  {
+    "revision": "89d0374d234764cc5864bada6e5bb6be",
+    "url": "/static/media/BF396DF7D0C0-Bold.89d0374d.otf"
+  },
+  {
+    "revision": "3ba34689852d2ed1678cb0e592e7d63d",
+    "url": "/static/media/Didot-Bold.3ba34689.ttf"
+  },
+  {
+    "revision": "1947e8264d85c086bb5333b95cbb061c",
+    "url": "/static/media/StartScreen_elConstellationscosmosdark982378_59726.1947e826.jpg"
+  },
+  {
+    "revision": "0be29161687f640dfa669a7d0bda26e7",
+    "url": "/static/media/StartScreen_elImage_683957.0be29161.png"
+  },
+  {
+    "revision": "d2c14174ed9c372ad4175846716f55bc",
+    "url": "/static/media/StartScreen_elImage2_677076.d2c14174.png"
+  },
+  {
+    "revision": "7c50d59ca1e2c90a6d1c",
+    "url": "/static/js/main.cefcb420.chunk.js"
+  },
+  {
+    "revision": "7fb6f06d22910158682d8689d86ad355",
+    "url": "/static/media/StartScreen_elImage4_395107.7fb6f06d.png"
+  },
+  {
+    "revision": "26607e2138c4530bff349c96ec1099e5",
+    "url": "/static/media/StartScreen_elImage5_744523.26607e21.png"
+  },
+  {
+    "revision": "80179a93a984ced9d4fe87f395346f0f",
+    "url": "/static/media/StartScreen_elImage6_174671.80179a93.png"
+  },
+  {
+    "revision": "1109becbce113c3c6268d75a4271ea58",
+    "url": "/static/media/StartScreen_elImage7_646092.1109becb.png"
+  },
+  {
+    "revision": "a8f884ad8b56ec4843f957bfb7c6fbe7",
+    "url": "/static/media/StartScreen_elImage8_898929.a8f884ad.png"
+  },
+  {
+    "revision": "3bc609c5f29261830791f5957f207dc2",
+    "url": "/static/media/StartScreen_elImage9_681875.3bc609c5.png"
+  },
+  {
+    "revision": "ede42a99be2602ef1df20f65c3d158a2",
+    "url": "/static/media/StartScreen_elImage10_425783.ede42a99.png"
+  },
+  {
+    "revision": "a26c892b673b73a7bb91e9195ff0583e",
+    "url": "/static/media/StartScreen_elImage11_742228.a26c892b.png"
+  },
+  {
+    "revision": "f7ff9b7a4eee9ec96ef62cfe1e67e037",
+    "url": "/static/media/StartScreen_elImage12_434954.f7ff9b7a.png"
+  },
+  {
+    "revision": "06c94a4cb621034540ae06f47678bd91",
+    "url": "/index.html"
+  }
+];
